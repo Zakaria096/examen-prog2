04682125a4c08cd77b33f824d8c8b81e123f50b3
Zakaria096 <hei.zakaria.3@gmail.com>
2024-07-31 10:17:53 +0300

    do some change

---
+++ examenProg2/src/songs/java/playlist.java
@@ -0,0 +1,5 @@
+package songs.java;
+
+public class playlist {
+
+}
--- examenProg2/src/songs/java/songs.java
+++ examenProg2/src/songs/java/songs.java
@@ -1,11 +1,13 @@
-package songs.java;
-
 import java.util.List;
 
 public class songs {
-   List<String> chansons;
 
-    public songs(List<String> chansons) {
-        this.chansons = chansons;
+
+    public class songs {
+        List<String> chansons;
+
+        public songs(List<String> chansons) {
+            this.chansons = chansons;
+        }
     }
 }
